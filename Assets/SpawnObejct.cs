1ec51ef0dccf77da7adf15d17ce40a340205db8a
Eisenblock <72503185+Eisenblock@users.noreply.github.com>
2025-06-24 10:32:10 +0200

    UPdateTemplate COde

--- Assets/SpawnObejct.cs
+++ Assets/SpawnObejct.cs
@@ -3,26 +3,64 @@
 public class SpawnObejct : MonoBehaviour
 {
     public GameObject[] templatePrefabs;
+    public GameObject[] templatelvl1;
+    public GameObject[] templatelvl2;
     public float spawnInterval = 80f; // z.B. alle 5 Sekunden ein Objekt
-    public float speedIncreaseInterval = 30f; // alle 20 Sekunden schneller
+    public float speedIncreaseInterval = 19f; // alle 20 Sekunden schneller
     public float fallSpeed = 4f;
-
+    private bool lvl1 = false;
+    private bool lvl2 = false;
     private float spawnTimer = 80f;
     private float speedTimer = 0f;
     private int lastIndex = -1;
+    private int countLvl = 0;
 
     void Update()
     {
         spawnTimer += Time.deltaTime;
         Debug.Log($"SpawnTimer: {spawnTimer:F2} / {spawnInterval}");
         speedTimer += Time.deltaTime;
+        if (!lvl1 && !lvl2)
+        {
+            SpawnTemplates(templatePrefabs);
+        }
 
+        if (lvl1 && !lvl2)
+        {
+            SpawnTemplates(templatelvl1);
+        }
+
+
+        // Geschwindigkeit alle 20s erhöhen
+        if (speedTimer >= speedIncreaseInterval && countLvl <=1)
+        {
+            switch (countLvl)
+            {
+                case 0:
+                    lvl1 = true;
+                    lvl2 = false;
+                    break;
+                case 1:
+                    lvl1 = false;
+                    lvl2 = true;
+                    break;
+                default:
+                    lvl1 = false;
+                    lvl2 = false;
+                    break;
+            }
+            countLvl ++;
+        }
+    }
+
+    void SpawnTemplates(GameObject[] reftemps)
+    {
         // Objekt spawnen
         if (spawnTimer >= spawnInterval)
         {
             spawnTimer = 0f;
 
-            if (templatePrefabs == null || templatePrefabs.Length == 0)
+            if (reftemps == null || reftemps.Length == 0)
             {
                 Debug.LogWarning("Kein Prefab im Array!");
                 return;
@@ -31,11 +69,11 @@ public class SpawnObejct : MonoBehaviour
             int randomIndex;
             do
             {
-                randomIndex = Random.Range(0, templatePrefabs.Length);
+                randomIndex = Random.Range(0, reftemps.Length);
             } while (templatePrefabs.Length > 1 && randomIndex == lastIndex);
 
             lastIndex = randomIndex;
-            GameObject prefabToSpawn = templatePrefabs[randomIndex];
+            GameObject prefabToSpawn = reftemps[randomIndex];
 
             GameObject instance = Instantiate(prefabToSpawn, new Vector3(15, 0, 0), Quaternion.identity);
 
@@ -46,14 +84,5 @@ public class SpawnObejct : MonoBehaviour
                 trash.fallSpeed = fallSpeed;
             }
         }
-
-        // Geschwindigkeit alle 20s erhöhen
-       /*if (speedTimer >= speedIncreaseInterval)
-        {
-            speedTimer = 0f;
-            fallSpeed += 1f;
-            spawnInterval -= 4;
-            Debug.Log($"Fallgeschwindigkeit erhöht auf: {fallSpeed}");
-        }*/
     }
 }
